e2a9363efe60fdedb83419411f09399e13e5b52d
okosun <okosun.temple@gmail.com>
2021-01-15 11:37:40 -0500

    Added Test- Verify Stop Time element

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders start time element', () => {
   const linkElement = screen.getByText(/Start time/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders stop time element', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Stop time/i);
+  expect(linkElement).toBeInTheDocument();
+});
