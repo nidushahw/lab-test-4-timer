55993e4c0cdaf7a5f9f25fd803ecc5c06d47e989
okosun <okosun.temple@gmail.com>
2021-01-15 11:43:29 -0500

    Added Test- Verify Stop Time element

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// Completing the requirements for lab 5
 test('renders start time element', () => {
   render(<App />);
   const linkElement = screen.getByText(/Start time/i);
